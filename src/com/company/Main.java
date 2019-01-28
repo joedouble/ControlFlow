72dfb5acb4f0780ebb11a397718aceefacf833f2
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-01-28 13:13:19 -0500

    Committing code that completes all of the requirements of the assignment.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -24,10 +24,22 @@ public class Main {
         c = number.nextInt();
         System.out.println("You entered " + c + ".");
 
-        if (a < b && b < c ) {
-
-
+        if (a > b && a > c) {
+            if(b > c) {
+                System.out.print("The numbers from smallest to largest are : " + c + ", " + b + ", " + a);
+            } else
+                System.out.print("The numbers from smallest to largest are : " + b + ", " + c + ", " + a);
+        } else if (b > a && b > c) {
+            if (a > c) {
+                System.out.print("The numbers from smallest to largest are : " + c + ", " + a + ", " + b);
+            } else {
+                System.out.print("The numbers from smallest to largest are : " + a + ", " + c + ", " + b);
+            }
+        } else if (c > a && c > b) {
+            if (a > b) {
+                System.out.print("The numbers from smallest to largest are : " + b + ", " + a + ", " + c);
+            } else
+                System.out.print("The numbers from smallest to largest are : " + a + ", " + b + ", " + c);
         }
-
     }
 }
